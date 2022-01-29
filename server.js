8e04ed342ef5b07511e04c58dc1350e2341a6f45
joeprabawa <joeprabawa@gmail.com>
2022-01-29 12:31:56 +0800

    change port & host

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const path = require('path')
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
-app.listen(PORT, () =>
-  console.log(`Server running on http://${HOST}:${PORT}`, 'hello')
+app.listen(process.env.PORT || PORT, () =>
+  console.log(`Server running on http://${process.env.HOST || HOST}:${PORT}`, 'hello')
 );
